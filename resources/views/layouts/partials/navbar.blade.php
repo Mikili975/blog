621b5c043c15ef5a65ca32909c6ed268d9e1b88a
Miroslav Vlajnic <mikili975@outlook.com>
2017-08-15 01:19:43 +0200

    Druga probna promena

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -7,6 +7,7 @@
             <a class="nav-link" href="#">Neki link</a>
             <a class="nav-link" href="#">Neki link</a>
             <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="#">Admin</a>
         </nav>
     </div>
 </div>
