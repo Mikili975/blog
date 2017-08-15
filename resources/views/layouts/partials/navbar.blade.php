39ff14a1a184cd83470234471d9c758dc1eec664
Mikili975 <mikili975@outlook.com>
2017-08-15 04:09:50 +0200

    Sedma promena

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -5,7 +5,7 @@
             <a class="nav-link" href="#">Neki link</a>
             <a class="nav-link" href="#">Neki link</a>
             <a class="nav-link" href="#">Bezzze</a>
-            <a class="nav-link" href="#">666</a>
+            <a class="nav-link" href="#">777</a>
             <a class="nav-link" href="#">About</a>
             <a class="nav-link" href="#">Admin</a>
         </nav>
