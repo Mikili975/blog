43c06828340365a601ef21dda370e7b6b60d86bf
Mikili975 <mikili975@outlook.com>
2017-08-15 03:57:06 +0200

    Peta promena

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -4,7 +4,7 @@
             <a class="nav-link active" href="/">Home</a>
             <a class="nav-link" href="#">Neki link</a>
             <a class="nav-link" href="#">Neki link</a>
-            <a class="nav-link" href="#">Bezze</a>
+            <a class="nav-link" href="#">Bezzze</a>
             <a class="nav-link" href="#">About</a>
             <a class="nav-link" href="#">Admin</a>
         </nav>
